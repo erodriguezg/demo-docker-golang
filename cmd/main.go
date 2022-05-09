7b40969b16ca7fd97fcdd7e26b522d7de44f0920
Eduardo Rodríguez <erodriguez.cl@gmail.com>
2022-05-09 10:57:24 -0400

    base golang project

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
-	
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(ctx *gin.Context) {
+
+		name, ok := ctx.GetQuery("name")
+		if !ok {
+			name = "unknown"
+		}
+
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"Name": name,
+		})
+
+	})
+	r.Run()
 
 }
-
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/erodriguezg/demo-docker-golang
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.7
